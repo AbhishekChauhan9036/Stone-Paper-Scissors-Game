c75cd3e30548cd6cdd2785788a5d92b4a5b50713
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-02-27 14:38:53 +0530

    Code Formate Change

--- game.cpp
+++ game.cpp
@@ -1,76 +1,76 @@
 #include <iostream>
-#include <cstdlib> 
+#include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
-class Game{
+class Game {
 private:
     int playerChoice;
     int computerChoice;
+
 public:
-    Game(){
+    Game() {
         srand(time(0));
     }
 
-    void getPlayerChoice(){
-        cout<<"Hello Player!"<<endl;
-        cout<<"\nChoose the option:- \n";
-        cout<<" 1.Stone\n 2.Paper\n 3.Scissors\n";
-        cout<<"Enter Your Choice(1-3):- ";
-        cin>>playerChoice;
+    void getPlayerChoice() {
+        cout << "Hello Player!" << endl;
+        cout << "\nChoose the option:- \n";
+        cout << " 1. Stone\n 2. Paper\n 3. Scissors\n";
+        cout << "Enter Your Choice (1-3): ";
+        cin >> playerChoice;
 
-        if(playerChoice<1 or playerChoice>3){
-            cout<<"Please Choose Between 1-3:- "<<endl;
+        while (playerChoice < 1 || playerChoice > 3) {
+            cout << "Invalid choice! Please choose between 1-3: ";
+            cin >> playerChoice;
         }
     }
 
-    void getComputerChoice(){
-        computerChoice = rand()%3+1;
+    void getComputerChoice() {
+        computerChoice = rand() % 3 + 1;
     }
 
-    string choiceToString(int choice){
-        switch(choice){
-            case 1:
-                return "Stone";
-            case 2:
-                return "Paper";
-            case 3:
-                return "Scissors";
-            default:
-                return "Unknown";
+    string choiceToString(int choice) {
+        switch (choice) {
+            case 1: return "Stone";
+            case 2: return "Paper";
+            case 3: return "Scissors";
+            default: return "Unknown";
         }
     }
 
-    void determineWinner(){
-        cout<<"\nYou Choose: "<<choiceToString(playerChoice)<<endl;
-        cout<<"Computer Choose: "<<choiceToString(computerChoice)<<endl;
+    void determineWinner() {
+        cout << "\nYou Chose: " << choiceToString(playerChoice) << endl;
+        cout << "Computer Chose: " << choiceToString(computerChoice) << endl;
 
-        if(playerChoice == computerChoice){
-            cout<<"Game is Draw  "<<endl;
-        }else if((playerChoice == 1 and computerChoice == 3) or
-                (playerChoice == 2 and computerChoice == 1) or
-                (playerChoice == 3 and computerChoice == 2)){
-            cout<<endl<<"Congratulations You Win!... can you want to try and win😀"<<endl<<endl;
-        }else{
-            cout<<endl<<"Computer Win! Better Luck Next Time bad luck 😢"<<endl<<endl;
+        if (playerChoice == computerChoice) {
+            cout << "Game is a Draw!" << endl;
+        } else if ((playerChoice == 1 && computerChoice == 3) ||
+                   (playerChoice == 2 && computerChoice == 1) ||
+                   (playerChoice == 3 && computerChoice == 2)) {
+            cout << "\nCongratulations! You Win! 😀" << endl;
+        } else {
+            cout << "\nComputer Wins! Better Luck Next Time. 😢" << endl;
         }
     }
 };
 
 int main() {
     char playAgain;
-    do{
+    do {
         Game g1;
         g1.getPlayerChoice();
         g1.getComputerChoice();
         g1.determineWinner();
 
-        cout<<"Do you want to play again?(y/n): ";
-        cin>>playAgain;
-    }while(playAgain == 'Y' or playAgain == 'y');
+        cout << "Do you want to play again? (y/n): ";
+        cin >> playAgain;
+    } while (playAgain == 'Y' || playAgain == 'y');
 
-    cout<<"Thanks for Playing.  Visit Again..."<<endl;
-    cout<<"We hope to see you again"<<endl;
-    cout<<"We will surely look forward to your next visit"<<endl;
+    cout << "Thanks for Playing. Visit Again!" << endl;
+    cout << "We hope to see you again!" << endl;
+    cout << "We will surely look forward to your next visit." << endl;
+    
     return 0;
 }
